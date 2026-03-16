c765c7f3cf7140a38f0a76e8e9f021f8d996c894
afquintana <mail@afquintana.com>
2026-03-16 18:40:01 +0100

    add maven publish plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.maven.publish)
 }
 
 android {
@@ -16,8 +17,6 @@ android {
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
@@ -38,4 +37,42 @@ android {
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
+}
+
+
+group = providers.gradleProperty("GROUP").get()
+version = providers.gradleProperty("VERSION_NAME").get()
+
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
+
+    pom {
+        name.set(providers.gradleProperty("POM_NAME"))
+        description.set(providers.gradleProperty("POM_DESCRIPTION"))
+        inceptionYear.set(providers.gradleProperty("POM_INCEPTION_YEAR"))
+        url.set(providers.gradleProperty("POM_URL"))
+
+        licenses {
+            license {
+                name.set(providers.gradleProperty("POM_LICENSE_NAME"))
+                url.set(providers.gradleProperty("POM_LICENSE_URL"))
+                distribution.set(providers.gradleProperty("POM_LICENSE_DIST"))
+            }
+        }
+
+        developers {
+            developer {
+                id.set(providers.gradleProperty("POM_DEVELOPER_ID"))
+                name.set(providers.gradleProperty("POM_DEVELOPER_NAME"))
+                url.set(providers.gradleProperty("POM_DEVELOPER_URL"))
+            }
+        }
+
+        scm {
+            url.set(providers.gradleProperty("POM_SCM_URL"))
+            connection.set(providers.gradleProperty("POM_SCM_CONNECTION"))
+            developerConnection.set(providers.gradleProperty("POM_SCM_DEV_CONNECTION"))
+        }
+    }
 }
